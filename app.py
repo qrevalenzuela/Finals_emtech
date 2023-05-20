6cabe52ca3cbc4217ad56f0dccdf89efa308f4fc
qrevalenzuela <112681441+qrevalenzuela@users.noreply.github.com>
2023-05-20 14:37:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,32 @@ from PIL import Image
 import numpy as np
 model = tf.keras.models.load_model('C:\\Users\\valen\\Downloads\\BEST_MODEL_STRONG.h5')
 
-def predict(image)
+def predict(image):
+    # Preprocess the image
     processed_image = preprocess_image(image)
-   
+    
+    # Make prediction using the model
     prediction = model.predict(processed_image)
-  
+    
+    # Return the prediction
     return prediction
 def preprocess_image(image):
-  
+    # Resize the image to the expected input shape of the model
     image = image.resize((64, 64))
     
-
+    # Convert the image to a numpy array
     image_array = np.array(image)
     
-  
+    # Normalize the image
     image_array = image_array / 255.0
     
-   
+    # Add an extra dimension to match the input shape of the model
     processed_image = np.expand_dims(image_array, axis=0)
     
     return processed_image
 def main():
-    st.title("Multi-class Weather Prediction By Robin E. Valenzuela")
-    st.write("Upload an image and we shall predict.")
+    st.title("Multi-class Weather Prediction by Robin E. Valenzuela")
+    st.write("Upload an image and click 'Predict' to see what happens.")
 
     uploaded_image = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
